5bb6cbaf8eebb0686023eb04a1725f1f494d06f7
bombrun.nicolas@gmail.com <Nicolas42290!>
2022-04-28 12:35:42 +0200

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { JwtInterceptor } from './config/JwtInterceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
